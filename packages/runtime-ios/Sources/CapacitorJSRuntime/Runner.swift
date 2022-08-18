fd51ef84ffd5a1ccf100f2249562defdfd932ec9
Joseph Pender <joey@ionic.io>
2022-08-18 09:46:02 -0500

    Adding setTimeout/setInterval

--- packages/runtime-ios/Sources/CapacitorJSRuntime/Runner.swift
+++ packages/runtime-ios/Sources/CapacitorJSRuntime/Runner.swift
@@ -2,11 +2,11 @@ import JavaScriptCore
 
 public class Runner {
     private var machine: JSVirtualMachine
-    private var eventListeners: Dictionary<String, JSValue>
+    private var eventListeners = Dictionary<String, JSValue>()
+    private var timers = Dictionary<Int, Timer>()
     
     public init() {
         self.machine = JSVirtualMachine()
-        self.eventListeners = Dictionary<String, JSValue>()
     }
     
     private func initContext() throws -> JSContext {
@@ -27,12 +27,44 @@ public class Runner {
             self.eventListeners.removeValue(forKey: type)
         }
         
+        let setTimeout: @convention(block) (JSValue, Int) -> Int = { callback, timeout in
+            let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(timeout / 1000), repeats: false) { t  in
+                callback.call(withArguments: [])
+                self.timers.removeValue(forKey: t.hashValue)
+            }
+            
+            RunLoop.current.add(timer, forMode: .common)
+            
+            self.timers[timer.hashValue] = timer
+            return timer.hashValue
+        }
+        
+        let clearTimeout: @convention(block) (Int) -> Void = { timeoutId in
+            if let timer = self.timers.removeValue(forKey: timeoutId) {
+                timer.invalidate()
+            }
+        }
+        
+        let setInterval: @convention(block) (JSValue, Int) -> Int = { callback, timeout in
+            let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(timeout / 1000), repeats: true) { t  in
+                callback.call(withArguments: [])
+            }
+            
+            RunLoop.current.add(timer, forMode: .common)
+            
+            self.timers[timer.hashValue] = timer
+            return timer.hashValue
+        }
+        
         let newCustomEvent: @convention(block) (String) -> JSCustomEvent = JSCustomEvent.init
         
         newContext.setObject(JSConsole.self, forKeyedSubscript: "console" as NSString)
         newContext.setObject(addEventListener, forKeyedSubscript: "addEventListener" as NSString)
         newContext.setObject(removeEventListener, forKeyedSubscript: "removeEventListener" as NSString)
-        
+        newContext.setObject(setTimeout, forKeyedSubscript: "setTimeout" as NSString)
+        newContext.setObject(clearTimeout, forKeyedSubscript: "clearTimeout" as NSString)
+        newContext.setObject(setInterval, forKeyedSubscript: "setInterval" as NSString)
+        newContext.setObject(clearTimeout, forKeyedSubscript: "clearInterval" as NSString)
         newContext.setObject(newCustomEvent, forKeyedSubscript: "CustomEvent" as NSString)
 
         return newContext
