8db2ac6b5470dc981aaf2ab0d91251fbe2d10d07
Joseph Pender <joey@ionic.io>
2023-04-18 14:11:15 -0500

    Using callback to signal worker completion

--- packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/RunnerWorker.kt
+++ packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/RunnerWorker.kt
@@ -1,12 +1,25 @@
 package io.ionic.backgroundrunner.plugin;
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import io.ionic.backgroundrunner.JSFunction
 import io.ionic.backgroundrunner.Runner
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
+import org.json.JSONObject
 
 class RunnerWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    private val runner: Runner
+    private var runnerContext: io.ionic.backgroundrunner.Context? = null
+
+    init {
+        this.runner = Runner()
+    }
+
     override fun doWork(): Result {
         try {
             val label = this.inputData.getString("label") ?: ""
@@ -17,13 +30,24 @@ class RunnerWorker(context: Context, workerParams: WorkerParameters) : Worker(co
                 throw Exception("label is empty")
             }
 
-            val runner = Runner()
-            val runnerContext = runner.createContext(label)
+            val srcFile = this.applicationContext.assets.open("public/${src}").bufferedReader().use {
+                it.readText()
+            }
 
-            runnerContext.execute("console.log('hello world from a android background task!');", false)
+            this.runnerContext = this.runner.createContext(label)
+            this.runnerContext?.execute(srcFile, false)
 
-            runner.destroy()
-            return Result.success()
+            var success = false
+
+            runBlocking {
+                success = this@RunnerWorker.execute(event)
+            }
+
+            if (success) {
+                return Result.success();
+            }
+
+            return Result.failure()
         } catch (ex: Exception) {
             val data = Data.Builder()
                 .putString("error", ex.toString())
@@ -31,6 +55,34 @@ class RunnerWorker(context: Context, workerParams: WorkerParameters) : Worker(co
 
             return Result.failure(data)
         }
+    }
 
+    private suspend fun execute(event: String): Boolean {
+        val result = MutableStateFlow<Boolean?>(null)
+        class CompletionCallback: JSFunction(args = null) {
+            override fun run() {
+                super.run()
+
+                Log.d("BackgroundRunner", "Callback triggered")
+                result.value = true
+            }
+        }
+
+        val callback = CompletionCallback()
+        this.runnerContext?.registerFunction("_backgroundCallback", callback)
+
+
+        val details = JSONObject()
+        details.put("completed", "__ebr::_backgroundCallback")
+
+        this.runnerContext?.dispatchEvent(event, details)
+
+        val success = result.conditionalAwait {
+            it != null
+        }
+
+        this.runner.destroy()
+
+        return success!!
     }
 }
