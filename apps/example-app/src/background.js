b619f2caf348773393ed6573e47eed7c1f01802f
Joseph Pender <joey@ionic.io>
2023-04-20 11:56:07 -0500

    Adding iOS project

--- apps/example-app/src/background.js
+++ apps/example-app/src/background.js
@@ -1,3 +1,7 @@
 addEventListener("updateSystem", () => {
   console.log("do something to update the system here");
 });
+
+addEventListener("updateSystemThrow", () => {
+  throw new Error("an error was thrown from javascript");
+});
