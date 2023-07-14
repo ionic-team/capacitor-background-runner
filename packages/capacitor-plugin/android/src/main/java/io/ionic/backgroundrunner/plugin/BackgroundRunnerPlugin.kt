209a04b0853418b3396ee850963f10181441cf7f
Joseph Pender <joey@ionic.io>
2023-07-13 22:41:08 -0500

    Starting work to bring android plugin to parity with iOS

--- packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/BackgroundRunnerPlugin.kt
+++ packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/BackgroundRunnerPlugin.kt
@@ -15,111 +15,59 @@ import com.getcapacitor.annotation.CapacitorPlugin
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 @CapacitorPlugin(name = "BackgroundRunner")
 class BackgroundRunnerPlugin: Plugin() {
-    private var configs: HashMap<String, RunnerConfig> = HashMap()
+    private var impl: BackgroundRunner? = null
 
     override fun load() {
-        val runnerConfigs = config.configJSON.getJSONArray("runners")
-
-        for (i in 0 until runnerConfigs.length()) {
-            val configJSON = runnerConfigs.getJSONObject(i)
-
-            val runnerConfig = RunnerConfig(
-                configJSON.getString("label"),
-                configJSON.getString("src"),
-                configJSON.getString("event"),
-                configJSON.getBoolean("repeat"),
-                configJSON.optBoolean("autoStart", false),
-                configJSON.getInt("interval"),
-            )
-
-            configs[runnerConfig.label] = runnerConfig
-        }
+        super.load()
+        impl = BackgroundRunner.getInstance(this.context)
 
         bridge.app.setStatusChangeListener {
             if (!it) {
                 Log.d("Background Runner", "registering runner workers")
-                this.initRunnerWorkers()
+                impl?.scheduleBackgroundTask(this.context)
             }
         }
     }
 
     @PluginMethod
     fun dispatchEvent(call: PluginCall) {
-        val runnerLabel = call.getString("label") ?: throw Exception("label is missing")
-        val runnerEvent = call.getString("event") ?: throw Exception("event is missing")
+        try {
+            val impl = BackgroundRunner.getInstance(this.context)
 
-        val details = call.getObject("details", JSObject())!!
+            val runnerEvent = call.getString("event") ?: throw Exception("event is missing or invalid")
+            val details = call.getObject("details")
+            val config = impl.config ?: throw Exception("no runner config loaded")
+            config.event = runnerEvent
 
-        val config = this.configs[runnerLabel] ?: throw Exception("no runner config found for label")
+            GlobalScope.launch(Dispatchers.Default) {
+                try {
+                    val returnData = impl.execute(
+                        this@BackgroundRunnerPlugin.context,
+                        config,
+                        details
+                    )
 
-        GlobalScope.launch(Dispatchers.Default) {
-            try {
-                val returnData = executeRunner(config, this@BackgroundRunnerPlugin.bridge.context, details, runnerEvent)
-
-                if (returnData != null) {
-                    call.resolve(JSObject.fromJSONObject(returnData))
-                } else {
-                    call.resolve()
+                    if (returnData != null) {
+                        call.resolve(JSObject.fromJSONObject(returnData))
+                    } else {
+                        call.resolve()
+                    }
+                } catch (ex: Exception) {
+                    call.reject(ex.message)
                 }
-            } catch (ex: Exception) {
-                call.reject(ex.message)
             }
+        } catch(ex: Exception) {
+            call.reject(ex.message)
         }
     }
 
     @PluginMethod
     fun registerBackgroundTask(call: PluginCall) {
-        try {
-            val newConfigJSON = call.getObject("runner") ?: throw Exception("a runner config is required")
-
-            val label = newConfigJSON.getString("label") ?: throw Exception("runner label is missing or invalid")
-            val src = newConfigJSON.getString("src") ?: throw Exception("runner src is missing or invalid")
-            val event = newConfigJSON.getString("event") ?: throw Exception("runner event is missing or invalid")
-            val repeat = newConfigJSON.getBoolean("repeat", false) ?: false
-            val interval = newConfigJSON.getInt("interval")
-
-            val newConfig = RunnerConfig(label, src, event, repeat, true, interval)
-
-            configs[newConfig.label] = newConfig
-
-            call.resolve()
-        } catch (ex: Exception) {
-            call.reject(ex.message)
-        }
-    }
-
-    private fun initRunnerWorkers() {
-        configs.forEach {
-            if (it.value.autoStart) {
-                scheduleRunnerTask(it.value)
-            }
-        }
-    }
-
-    private fun scheduleRunnerTask(runnerConfig: RunnerConfig) {
-        val data = Data.Builder()
-            .putString("label", runnerConfig.label)
-            .putString("src", runnerConfig.src)
-            .putString("event", runnerConfig.event)
-            .build()
-
-        if (!runnerConfig.repeat) {
-            val work = OneTimeWorkRequest.Builder(RunnerWorker::class.java)
-                .setInitialDelay(runnerConfig.interval.toLong(), TimeUnit.MINUTES)
-                .setInputData(data)
-                .addTag(runnerConfig.label)
-                .build()
-            WorkManager.getInstance(this.context).enqueueUniqueWork(runnerConfig.label, ExistingWorkPolicy.REPLACE, work)
-        } else {
-            val work = PeriodicWorkRequest.Builder(RunnerWorker::class.java, runnerConfig.label.toLong(), TimeUnit.MINUTES)
-                .setInputData(data)
-                .addTag(runnerConfig.label)
-                .build()
-            WorkManager.getInstance(this.context).enqueueUniquePeriodicWork(runnerConfig.label, ExistingPeriodicWorkPolicy.UPDATE, work)
-        }
+        call.resolve()
     }
 }
--- packages/capacitor-plugin/scripts/copy-native-sdks.sh
+++ packages/capacitor-plugin/scripts/copy-native-sdks.sh
@@ -3,9 +3,5 @@
 rm -r ./ios/Plugin/RunnerEngine
 cp -R ../ios-engine/Sources/RunnerEngine ./ios/Plugin/
 
-rm -r ./android/android-engine
-mkdir -p ./android/android-engine/src
-
-cp -f ../android-engine/build.gradle ./android/android-engine/build.gradle
-cp -f ../android-engine/settings.gradle ./android/android-engine/settings.gradle
-cp -R ../android-engine/src/ ./android/android-engine/src/
+rm -r ./android/android-js-engine
+cp -R ../android-engine/android-js-engine ./android/
