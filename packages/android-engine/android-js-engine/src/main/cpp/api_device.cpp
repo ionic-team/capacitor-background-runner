b714871a0c559f432f875dd030f73f092e9de645
Joseph Pender <joey@ionic.io>
2023-07-17 00:06:22 -0500

    Implementing network status api

--- packages/android-engine/android-js-engine/src/main/cpp/api_device.cpp
+++ packages/android-engine/android-js-engine/src/main/cpp/api_device.cpp
@@ -35,5 +35,30 @@ JSValue api_device_battery(JSContext *ctx, JSValueConst this_val, int argc, JSVa
 
 JSValue api_device_network(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
 {
-    return JS_UNDEFINED;
+    JSValue ret_value, jni_exception = JS_UNDEFINED;
+
+    auto *parent_ctx = (Context *)JS_GetContextOpaque(ctx);
+    auto *env = parent_ctx->getJNIEnv();
+
+    auto *device = env->GetObjectField(parent_ctx->capacitor_api, parent_ctx->jni_classes->capacitor_api_device_field);
+
+    jmethodID j_device_network_method = env->GetMethodID(parent_ctx->jni_classes->device_api_class, "getNetworkStatus", "()Ljava/lang/String;");
+
+    auto *j_json_str = (jstring)env->CallObjectMethod(device, j_device_network_method);
+    jni_exception = check_and_throw_jni_exception(env, ctx);
+    if (JS_IsException(jni_exception)) {
+        return jni_exception;
+    }
+
+    if (j_json_str == nullptr) {
+        return JS_NULL;
+    }
+
+    const auto *c_json_str = env->GetStringUTFChars(j_json_str, nullptr);
+
+    ret_value = parent_ctx->parseJSON(c_json_str);
+
+    env->ReleaseStringUTFChars(j_json_str, c_json_str);
+
+    return ret_value;
 }
--- packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/api/Device.kt
+++ packages/capacitor-plugin/android/src/main/java/io/ionic/backgroundrunner/plugin/api/Device.kt
@@ -1,16 +1,24 @@
 package io.ionic.backgroundrunner.plugin.api
 
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
 import android.os.BatteryManager
+import android.os.Build
 import io.ionic.android_js_engine.api.DeviceAPI
 import org.json.JSONObject
 
+
 class Device (context: android.content.Context) : DeviceAPI {
     private val context: android.content.Context
+    private val connectivityManager: ConnectivityManager
 
     init {
         this.context = context
+        this.connectivityManager = this.context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager;
     }
 
     override fun getBatteryStatus(): String {
@@ -22,9 +30,31 @@ class Device (context: android.content.Context) : DeviceAPI {
     }
 
     override fun getNetworkStatus(): String {
+        var status = NetworkStatus()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val activeNetwork: Network? = this.connectivityManager.activeNetwork
+            val capabilities: NetworkCapabilities? =
+                this.connectivityManager.getNetworkCapabilities(this.connectivityManager.activeNetwork)
+            if (activeNetwork != null && capabilities != null) {
+                status.connected =
+                    capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
+                            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    status.connectionType = NetworkStatus.ConnectionType.WIFI
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    status.connectionType = NetworkStatus.ConnectionType.CELLULAR
+                } else {
+                    status.connectionType = NetworkStatus.ConnectionType.UNKNOWN
+                }
+            }
+        } else {
+            status = getAndParseNetworkInfo()
+        }
+
         val networkInfo = JSONObject()
-        networkInfo.put("connected", true)
-        networkInfo.put("connectionType", "")
+        networkInfo.put("connected", status.connected)
+        networkInfo.put("connectionType", status.connectionType)
 
         return networkInfo.toString()
     }
@@ -49,5 +79,21 @@ class Device (context: android.content.Context) : DeviceAPI {
             return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL
         }
         return false
+    }
+
+    @SuppressWarnings("deprecation")
+    private fun getAndParseNetworkInfo(): NetworkStatus {
+        val networkStatus = NetworkStatus()
+        val networkInfo = connectivityManager.activeNetworkInfo
+        if (networkInfo != null) {
+            networkStatus.connected = networkInfo.isConnected
+            val typeName = networkInfo.typeName
+            if (typeName == "WIFI") {
+                networkStatus.connectionType = NetworkStatus.ConnectionType.WIFI
+            } else if (typeName == "MOBILE") {
+                networkStatus.connectionType = NetworkStatus.ConnectionType.CELLULAR
+            }
+        }
+        return networkStatus
     }
 }
