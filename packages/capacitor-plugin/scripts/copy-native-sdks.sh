5b87df6bcb2b1cbb5047680cee5af231b0d0b864
Joseph Pender <joey@ionic.io>
2023-07-17 20:34:15 -0500

    improving android-js-engine build scripts

--- packages/capacitor-plugin/scripts/copy-native-sdks.sh
+++ packages/capacitor-plugin/scripts/copy-native-sdks.sh
@@ -3,4 +3,5 @@
 rm -r ./ios/Plugin/RunnerEngine
 cp -R ../ios-engine/Sources/RunnerEngine ./ios/Plugin/
 
+rm -f ./android/src/main/libs/android-js-engine-release.aar
 cp -R ../android-engine/android-js-engine/build/outputs/aar/android-js-engine-release.aar ./android/src/main/libs
