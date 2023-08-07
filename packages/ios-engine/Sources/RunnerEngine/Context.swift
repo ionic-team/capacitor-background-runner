b8a35fd87d410e3f84256449e7fd411fe04f53aa
Joseph Pender <joey@ionic.io>
2023-08-07 17:33:32 -0500

    iOS: Setting thread QoS

--- packages/ios-engine/Sources/RunnerEngine/Context.swift
+++ packages/ios-engine/Sources/RunnerEngine/Context.swift
@@ -32,6 +32,7 @@ public class Context {
         }
         
         thread.name = "[\(name)] context thread"
+        thread.qualityOfService = .userInitiated
         thread.start()
 
         try setupWebAPI()
