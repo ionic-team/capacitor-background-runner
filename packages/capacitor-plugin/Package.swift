4ffc685ee202899e684c0bd9a80ebcca4dec0260
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-11-27 13:33:44 +0000

    chore(ios)!: Update deployment target
    BREAKING CHANGE: iOS deployment target is now 15

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorBackgroundRunner",
-    platforms: [.iOS(.v14), .macOS(.v13)],
+    platforms: [.iOS(.v15), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
