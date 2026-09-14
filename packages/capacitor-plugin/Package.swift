f54a32c0fb163df5dde17673e13817298b47d32a
jcesarmobile <jcesarmobile@gmail.com>
2026-09-14 18:57:45 +0200

    feat!: align plugin baselines with core (#162)
    * chore: migrate example apps to Capacitor 9 alpha
    
    * chore!: Align plugin baselines with core
    
    
    Co-authored-by: OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorBackgroundRunner",
-    platforms: [.iOS(.v15), .macOS(.v13)],
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -23,7 +23,6 @@ let package = Package(
             name: "CapacitorBackgroundRunner",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
                 "RunnerEngine"
             ],
             path: "ios/Sources/CapacitorBackgroundRunner"
